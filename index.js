1401cb188c3b5cde8906b0ef1c78080fb365a0f9
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-06-19 23:17:18 +0300

    commit tree fd3822aef6b5d731ab90d1182e99c0e7bb65a205

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+function submitData(name, email) {
+    const info = { name: name,
+        email: email };
+
+    const obj = {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        }
+    }
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            document.body.innerHTML = data.id;
+        })
+        .catch(error => {
+
+            document.body.innerHTML = error.message;
+        });
+}
